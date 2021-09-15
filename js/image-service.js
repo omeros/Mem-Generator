dc0896b76683dc743755ab92ad449aa6794f6774
omeros <omer.golan.dev@gmail.com>
2021-09-15 16:44:12 +0300

    upload imgaes to claudinary

--- js/image-service.js
+++ js/image-service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const gImages = [{ id : 1,img : 'img/img1.jpg'},{ id : 2,img : 'img/img2.jpg'},{ id : 3,img : 'img/img3.jpg'},{id : 4,img : 'img/img4.jpg'},{id: 5,img: 'img/img5.jpg'},{id: 6,img: 'img/img6.jpg'},{id : 7,img : 'img/img7.jpg'},{id : 8,img : 'img/img8.jpg'},{ id : 9,img : 'img/img9.jpg'}]
+const gImages = [{ id : 1,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713128/MemeGenerator/img1_spagwn.jpg'},{ id : 2,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713133/MemeGenerator/img2_j37iw1.jpg'},{ id : 3,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713137/MemeGenerator/img3_ddeftn.jpg'},{id : 4,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713139/MemeGenerator/img4_xytyra.jpg'},{id: 5,img: 'https://res.cloudinary.com/omerphoto/image/upload/v1631713142/MemeGenerator/img5_o0mj5w.jpg'},{id: 6,img: 'https://res.cloudinary.com/omerphoto/image/upload/v1631713144/MemeGenerator/img6_pvbw8c.jpg'},{id : 7,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713146/MemeGenerator/img7_jiu96m.jpg'},{id : 8,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713148/MemeGenerator/img8_njgwwt.jpg'},{ id : 9,img : 'https://res.cloudinary.com/omerphoto/image/upload/v1631713150/MemeGenerator/img9_wru22g.jpg'}]
 
 
 function getImages(){
